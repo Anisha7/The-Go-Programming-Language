ba7659511ec4191ca87a294cfe55f4fba8444e0e
Anisha Jain <anishajain@outlook.com>
2019-03-14 21:03:00 -0700

    testing

--- Chapter2/testPopcount.go
+++ Chapter2/testPopcount.go
@@ -13,21 +13,21 @@ func main() {
 	fmt.Println("Testing Version 1")
 	start := time.Now()
 	fmt.Println(popcount.PopCount(x))
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+	fmt.Printf("%.6fs elapsed\n", time.Since(start).Seconds())
 
 	fmt.Println("Testing Version 2")
 	start = time.Now()
 	fmt.Println(popcount.PopCount2(x))
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+	fmt.Printf("%.6fs elapsed\n", time.Since(start).Seconds())
 
 	fmt.Println("Testing Version 3")
 	start = time.Now()
 	fmt.Println(popcount.PopCount3(x))
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+	fmt.Printf("%.6fs elapsed\n", time.Since(start).Seconds())
 
 	fmt.Println("Testing Version 4")
 	start = time.Now()
 	fmt.Println(popcount.PopCount4(x))
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+	fmt.Printf("%.6fs elapsed\n", time.Since(start).Seconds())
 
 }
